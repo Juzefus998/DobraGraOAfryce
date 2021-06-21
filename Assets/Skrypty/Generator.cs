fc0fb90b7fe5296736344d29eee54dcf77b13eee
Coach110 <mariusz1-17@o2.pl>
2021-06-21 17:00:58 +0200

    Postać cd

--- Assets/Skrypty/Generator.cs
+++ Assets/Skrypty/Generator.cs
@@ -11,8 +11,8 @@ public class Generator : MonoBehaviour
     Vector3[] wierzcholki;
     int[] trojkaty;
 
-    public int xSize = 100;
-    public int zSize = 100;
+    public int xSize = 1000;
+    public int zSize = 1000;
 
     System.Random rand = new System.Random();
    
@@ -36,7 +36,7 @@ public class Generator : MonoBehaviour
         {
             for (int x = 0; x <= xSize; x++)
             {
-                float y = Mathf.PerlinNoise(x * .105f, z * .05f) * 10f;
+                float y = Mathf.PerlinNoise(x * .05f, z * .05f) * 05f;
                 wierzcholki[i] = new Vector3(x, y, z);
                 i++;
             }
