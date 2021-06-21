ba1c5e220a178f6e91f2c43d875d12d823a7060f
Juzefus998 <m.juzefus@student.po.edu.pl>
2021-06-21 11:33:38 +0200

    Hasztag Testowanie Generacji

---
+++ Assets/Skrypty/Generator.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class Generator : MonoBehaviour
+{
+    Mesh mesh;
+    Tree tree;
+
+    Vector3[] wierzcholki;
+    int[] trojkaty;
+
+    public int xSize = 100;
+    public int zSize = 100;
+
+    //public System.Random rand = new System.Random();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateMesh();
+    }
+
+    void CreateShape()
+    {
+        wierzcholki = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        int i = 0;
+        for(int z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float y = Mathf.PerlinNoise(x * .05f, z * .05f) * 04f;
+                wierzcholki[i] = new Vector3(x, y, z);
+                i++;
+            }
+        }
+
+        trojkaty = new int[xSize * zSize * 6];
+        int wierz = 0;
+        int troj = 0;
+        for (int z = 0; z < zSize; z++) {
+            for (int x = 0; x < xSize; x++)
+            {
+                trojkaty[troj + 0] = wierz + 0;
+                trojkaty[troj + 1] = wierz + xSize + 1;
+                trojkaty[troj + 2] = wierz + 1;
+                trojkaty[troj + 3] = wierz + 1;
+                trojkaty[troj + 4] = wierz + xSize + 1;
+                trojkaty[troj + 5] = wierz + xSize + 2;
+
+                wierz++;
+                troj += 6;
+            }
+            wierz++;
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = wierzcholki;
+        mesh.triangles = trojkaty;
+
+        mesh.RecalculateNormals();
+    }
+
+   
+}
